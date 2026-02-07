ff9c635a6698990d508192a3b08b9aa9d268fbea
David Gethings <dgethings@me.com>
2026-02-07 10:31:46 +0400

    fix: remove shevron before input box as it looked crap

--- src/pq/tui.py
+++ src/pq/tui.py
@@ -31,13 +31,13 @@ class QueryPrompt(Widget):
         cast(QueryApp, self.app).action_accept_query()
 
     def compose(self) -> ComposeResult:
-        with Horizontal():
-            yield Static("> ", id="prompt")
-            yield Input(
-                value=self.query_string,
-                placeholder="Enter Python expression, use '_' to access data",
-                id="query-input",
-            )
+        # with Horizontal():
+        #     yield Static("> ", id="prompt")
+        yield Input(
+            value=self.query_string,
+            placeholder="Enter Python expression, use '_' to access data",
+            id="query-input",
+        )
 
 
 class ResultDisplay(Static):
